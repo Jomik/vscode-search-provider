954713bcba1665b84e2550fae90400efe645708f
Jonas Damtoft <jonasdamtoft@hotmail.com>
2017-04-24 19:43:52 +0200

    Get folders function

--- extension.js
+++ extension.js
@@ -1,7 +1,7 @@
 const Lang  = imports.lang;
 const Main  = imports.ui.main;
 const GLib = imports.gi.GLib;
-const Json = imports.gi.Json;
+const Gio = imports.gi.Gio;
 
 var vscodeSearchProvider = null;
 var storage = null;
@@ -10,18 +10,37 @@ function debug(message) {
   global.log("VS Code Search Provider ~ " + message);
 }
 
+function getFolders() {
+  let json = null;
+  try {
+    json = JSON.parse(GLib.file_get_contents(storage)[1]);
+  } catch(e) {
+    // Return empty array.
+  }
+  
+  if (json) {
+    json = json["openedPathsList"];
+    if (json) {
+      json = json["folders"];
+      if (json) {
+        return json;
+      }
+    }
+  }
+  return [];
+}
+
 const VSCodeSearchProvider = new Lang.Class({
   Name: 'VS Code Search Provider',
   
   _init: function() {
-    global.log("_init")
-    this.parser = Json.json_parser_new();
+    global.log("_init");
   },
-
+  
   getInitialResultSet: function(terms, callback, cancellable) {
     debug("getInitialResultSet: " + terms.join(" "));
   },
-
+  
   activateResult: function(id, terms) {
     debug("activateResult id: " + id);
     debug("activateResult terms: " + terms.join(" "));
@@ -37,7 +56,6 @@ function enable() {
   if (!vscodeSearchProvider) {
     global.log("enabled");
     vscodeSearchProvider = new VSCodeSearchProvider();
-    //Main.overview.addSearchProvider(windowSearchProvider);
     Main.overview.viewSelector._searchResults._registerProvider(vscodeSearchProvider);
   }
 }
