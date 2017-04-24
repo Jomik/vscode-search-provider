7ddb9b1229b6985e0b9e4a6f19c0634e9f244f17
Jonas Damtoft <jonasdamtoft@hotmail.com>
2017-04-24 23:40:53 +0200

    Send metas, missing icons

--- extension.js
+++ extension.js
@@ -1,5 +1,5 @@
-const Lang  = imports.lang;
-const Main  = imports.ui.main;
+const Lang = imports.lang;
+const Main = imports.ui.main;
 const GLib = imports.gi.GLib;
 const Gio = imports.gi.Gio;
 
@@ -10,38 +10,87 @@ function debug(message) {
   global.log("VS Code Search Provider ~ " + message);
 }
 
-function getFolders() {
+function getFolderPaths() {
   let json = null;
   try {
     json = JSON.parse(GLib.file_get_contents(storage)[1]);
-  } catch(e) {
+  } catch (e) {
     // Return empty array.
   }
-  
+
   if (json) {
     json = json["openedPathsList"];
     if (json) {
       json = json["folders"];
       if (json) {
+        debug("getFolderPaths = " + json);
         return json;
       }
     }
   }
+  debug("getFolderPaths = " + json);
   return [];
+}
+
+function projectNameFromPath(path) {
+  let name = path.split('\\').pop().split('/').pop();
+  debug("projectNameFromPath: " + path + " -> " + name);
+  return name;
+}
+
+function pathToResultObject(path, index) {
+  debug("pathToResultObject(" + index + 1 + "): " + path)
+  return {
+    'id': index + 1,
+    'name': projectNameFromPath(path),
+    'description': "VSCode project",
+    'path': path,
+    'createIcon': function(size) {
+      debug("Icon for " + projectNameFromPath(path));
+    }
+  }
 }
 
 const VSCodeSearchProvider = new Lang.Class({
   Name: 'VS Code Search Provider',
-  
-  _init: function() {
+
+  _init: function () {
     global.log("_init");
+    this.id = 'VSCodeProjects';
   },
-  
-  getInitialResultSet: function(terms, callback, cancellable) {
-    debug("getInitialResultSet: " + terms.join(" "));
+
+  getInitialResultSet: function (terms, callback, cancellable) {
+    debug("getInitialResultSet: " + terms);
+    let search = terms.join(" ").toLowerCase();
+    this._results = getFolderPaths().map(pathToResultObject);
+    this._resultIds = [];
+    for (candidate of this._results) {
+      if (candidate.name.toLowerCase().indexOf(search) !== -1) {
+        this._resultIds.push(candidate.id);
+      }
+    }
+    debug("Found [" + this._resultIds + "] for " + search);
+    return callback(this._resultIds);
   },
-  
-  activateResult: function(id, terms) {
+
+  getSubsearchResultSet: function (previousResults, terms, callback, cancellable) {
+    debug("getSubsearchResultSet");
+    this.getInitialResultSet(terms, callback, cancellable);
+  },
+
+  getResultMetas: function (resultIds, callback) {
+    let resultMetas = this._results.filter(function (res) {
+      return resultIds.indexOf(res.id) !== -1;
+    });
+    debug("getResultMetas: " + resultMetas);
+    callback(resultMetas);
+  },
+
+  filterResults: function (results, max) {
+    return results.slice(0, max);
+  },
+
+  activateResult: function (id, terms) {
     debug("activateResult id: " + id);
     debug("activateResult terms: " + terms.join(" "));
   }
