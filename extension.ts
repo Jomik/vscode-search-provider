b20e3c920e2fab9542d43109268e3ab8a58c4790
François GUÉRIN <fguerin@ville-tourcoing.fr>
2020-03-21 10:32:50 +0100

    Adds VSCodium support

--- extension.ts
+++ extension.ts
@@ -67,6 +67,8 @@ const findVSCode = (): CodeAppInfo | null => {
     // Offical VSCode snap, see <https://github.com/Jomik/vscode-search-provider/pull/14>
     // and <https://snapcraft.io/code>
     ["code_code.desktop", "Code"],
+    // VSCodium support
+    ["codium.desktop", "VSCodium"],
     // TODO: Figure out what systems these desktop files are from.
     ["code.desktop", "Code"],
     ["visual-studio-code.desktop", "Code"]
